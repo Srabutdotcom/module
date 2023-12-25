836ea4b1a9790cf4066341819e89a2d1846a254e
handoko <srabutdotcom@gmail.com>
2023-12-25 08:09:31 +0700

    edit rollup.config.mjs

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import terser from '@rollup/plugin-terser';
+
+//import rolluppluginTerser from 'https://cdn.jsdelivr.net/npm/@rollup/plugin-terser@0.4.4/+esm';debugger;
+
+export default [
+  {
+    input: './modul.js',
+    output: {
+      //dir: 'dist',
+      file: 'dist/modul.bundle.js',
+      format: 'es', // Use ES module format,
+      //chunkFileNames: '[name]-[hash].js'
+    },
+    plugins: [
+      terser(), // Minify the output bundle
+    ],
+  },
+];
+
+// npm install --save-dev @rollup/plugin-terser
+//run code -->
+//npx rollup -c rollup.config.js
+//or
+//npx rollup -c
